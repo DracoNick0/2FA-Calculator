0370e543a0d70731d55ea155e0a0e43655d109a8
Nicholas Santos <santos_nick@outlook.com>
2024-10-03 11:01:39 -0700

    Initialization of classes that are planned to be used.

---
+++ 2FA-Calculator/2FA/2FAClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2FA_Calculator._2FA
+{
+    internal class _2FAClass
+    {
+    }
+}
