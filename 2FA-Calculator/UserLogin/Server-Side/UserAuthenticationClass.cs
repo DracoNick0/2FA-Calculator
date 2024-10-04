9f17dd5342d48df29e0b88a1bc89a28727e0ec45
Nicholas Santos <santos_nick@outlook.com>
2024-10-04 09:18:24 -0700

    Creating an account in UserAuthenticationClass now uses salt.

--- 2FA-Calculator/UserLogin/Server-Side/UserAuthenticationClass.cs
+++ 2FA-Calculator/UserLogin/Server-Side/UserAuthenticationClass.cs
@@ -4,6 +4,7 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace _2FA_Calculator.UserLogin
 {
@@ -45,8 +46,45 @@ namespace _2FA_Calculator.UserLogin
 
             using (StreamWriter sw = new StreamWriter(userLoginStorage, true))
             {
-                string line = username + "," + password;
+                int saltLength = 8;
+                string salt = GenerateSalt(saltLength);
+
+                string hashedPassword = ComputeSha256Hash(password + salt);
+
+                string line = username + "," + hashedPassword + "," + salt;
                 sw.WriteLine(line);
+            }
+        }
+
+        private string GenerateSalt(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            StringBuilder result = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return result.ToString();
+        }
+
+        private string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256 object
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Compute the hash - returns a byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));  // Convert byte to hex string
+                }
+                return builder.ToString();
             }
         }
     }
