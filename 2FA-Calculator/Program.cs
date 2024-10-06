86292b9aa8587b0ac2c4117b26d5c439691a7043
Nicholas Santos <santos_nick@outlook.com>
2024-10-05 22:45:40 -0700

    User can now exit out of program.

--- 2FA-Calculator/Program.cs
+++ 2FA-Calculator/Program.cs
@@ -1,42 +1,48 @@
 ﻿using _2FA_Calculator.ClientSide;
 
-Console.WriteLine("Please input number corresponding to option: ");
-Console.WriteLine("  1. Login");
-Console.WriteLine("  2. Create new account");
 
-string? userInput = Console.ReadLine();
-if (userInput != null)
+string? userInput = null;
+while (true)
 {
-    LoginInterface userLogin = new LoginInterface();
+    Console.WriteLine("Please input number corresponding to option: ");
+    Console.WriteLine("  1. Login");
+    Console.WriteLine("  2. Create new account");
+    Console.WriteLine("  3. Exit");
 
-    switch (userInput[0])
+    userInput = Console.ReadLine();
+    if (userInput != null)
     {
-        case '1':
-            userLogin.requestUserAndPass();
-            if (userLogin.authenticateUser())
-            {
-                Console.WriteLine("Username: " + userLogin.getUsername());
-                Console.WriteLine("Password: " + userLogin.getPassword());
-                Console.WriteLine("You're in! *v*\n");
+        LoginInterface userLogin = new LoginInterface();
 
-                CalculatorClass calculator = new CalculatorClass();
-                Console.WriteLine("Input a simple expression with two integers and an operator, no spaces.");
-
-                double? temp;
-                if ((temp = calculator.evaluateExpression(Console.ReadLine())) != null)
+        switch (userInput[0])
+        {
+            case '1':
+                userLogin.requestUserAndPass();
+                if (userLogin.authenticateUser())
                 {
-                    Console.WriteLine("Result = " + temp.ToString());
+                    Console.WriteLine("\nWelcome " + userLogin.getUsername());
+
+                    CalculatorClass calculator = new CalculatorClass();
+                    Console.WriteLine("Input a simple expression with two integers and an operator, no spaces.");
+
+                    double? temp;
+                    if ((temp = calculator.evaluateExpression(Console.ReadLine())) != null)
+                    {
+                        Console.WriteLine("Result = " + temp.ToString() + "\n");
+                    }
                 }
-            }
-            else
-            {
-                Console.WriteLine("Username: " + userLogin.getUsername());
-                Console.WriteLine("Password: " + userLogin.getPassword());
-                Console.WriteLine("Incorrect username or password! ;n;");
-            }
-            break;
-        case '2':
-            userLogin.createAccount();
-            break;
+                else
+                {
+                    Console.WriteLine("Username: " + userLogin.getUsername());
+                    Console.WriteLine("Password: " + userLogin.getPassword());
+                    Console.WriteLine("Incorrect username and/or password! ;n;\n");
+                }
+                break;
+            case '2':
+                userLogin.createAccount();
+                break;
+            case '3':
+                return 1;
+        }
     }
 }
