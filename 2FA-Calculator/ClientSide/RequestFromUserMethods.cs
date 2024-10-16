c1682bd1720152b66d95b430ee22a360ebbab56d
Nicholas Santos <santos_nick@outlook.com>
2024-10-15 22:31:56 -0700

    RequestFromUserMethods now has confirm, confirmIfInputIsCorrect, requestInputAndConf, andrequestInput.

--- 2FA-Calculator/ClientSide/RequestFromUserMethods.cs
+++ 2FA-Calculator/ClientSide/RequestFromUserMethods.cs
@@ -2,5 +2,81 @@
 {
     class RequestFromUserMethods
     {
+        public string requestInput(string nameOfInput)
+        {
+            string? userInput = null;
+
+            while (userInput == null || userInput == string.Empty)
+            {
+                while (userInput == null || userInput == string.Empty)
+                {
+                    Console.Write("Enter your desired " + nameOfInput + ": ");
+                    userInput = Console.ReadLine();
+
+                    if (userInput == null || userInput == string.Empty)
+                    {
+                        Console.WriteLine("\n" + nameOfInput + " not valid, try again.");
+                    }
+                }
+            }
+
+            return userInput;
+        }
+
+        public string requestInputAndConf(string nameOfInput)
+        {
+            bool userIsSatisfied = false;
+            string? userInput = null;
+
+            while (!userIsSatisfied || userInput == null || userInput == string.Empty)
+            {
+                while (userInput == null || userInput == string.Empty)
+                {
+                    Console.Write("Enter your desired " + nameOfInput + ": ");
+                    userInput = Console.ReadLine();
+
+                    if (userInput == null || userInput == string.Empty)
+                    {
+                        Console.WriteLine("\n" + nameOfInput + " not valid, try again.");
+                    }
+                }
+
+                userIsSatisfied = confirmIfInputIsCorrect(userInput);
+            }
+
+            return userInput;
+        }
+
+        public bool confirmIfInputIsCorrect(string input)
+        {
+            Console.WriteLine("Is \"" + input + "\" correct?");
+            return confirm();
+        }
+
+        public bool confirm()
+        {
+            string? input;
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input != null)
+                {
+                    if (input.Length == 1)
+                    {
+                        if (input.CompareTo("y") == 0 || input.CompareTo("Y") == 0)
+                        {
+                            return true;
+                        }
+                        else if (input.CompareTo("n") == 0 || input.CompareTo("N") == 0)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                Console.Write("Please enter y or n: ");
+            }
+        }
     }
 }
