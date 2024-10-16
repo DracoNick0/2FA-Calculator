c87843e47b84d824eebcc0cc68b02509b7216439
Nicholas Santos <santos_nick@outlook.com>
2024-10-16 08:56:02 -0700

    Not tested: cleared PersistentStorageManager, and added a populateDynamicStorage function that takes all data from txt file and populates the Dictionary of Dictionaries.

--- 2FA-Calculator/ServerSide/PersistentStorageManager.cs
+++ 2FA-Calculator/ServerSide/PersistentStorageManager.cs
@@ -1,142 +1,60 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace _2FA_Calculator.ServerSide
 {
     class PersistentStorageManager
     {
-        private Hasher hasher;
         private string storageFilePath;
 
         public PersistentStorageManager(string storageFilePath)
         {
             this.storageFilePath = storageFilePath;
-            this.hasher = new Hasher();
         }
 
-        public string getUserEmail(string username)
+        public Dictionary<string, Dictionary<string, string>>? populateDynamicStorage()
         {
-            string? line = string.Empty;
+            Dictionary<string, Dictionary<string, string>> result
+                = new Dictionary<string, Dictionary<string, string>>();
 
-            using (StreamReader sr = new StreamReader(storageFilePath))
+            if (this.storageFilePath != null && result != null)
             {
-                while ((line = sr.ReadLine()) != null)
-                {
-                    string[] tokens = line.Split(',');
+                string? line = string.Empty;
 
-                    // Find line with the username and email, return email
-                    if (tokens[0].CompareTo(username) == 0)
+                using (StreamReader sr = new StreamReader(storageFilePath))
+                {
+                    while ((line = sr.ReadLine()) != null)
                     {
-                        return tokens[3];
+                        string[] tokens = line.Split(',');
+
+                        Dictionary<string, string> newDict = new Dictionary<string, string>();
+                        if (newDict != null)
+                        {
+                            result[tokens[0]] = newDict;
+                            result[tokens[0]].Add("hashedPassword", tokens[1]);
+                            result[tokens[0]].Add("salt", tokens[2]);
+                            result[tokens[0]].Add("email", tokens[3]);
+                        }
                     }
                 }
+
+                return result;
+                
             }
 
-            return string.Empty;
+            return null;
         }
 
-        public bool createAccount(string username, string password, string email)
+        /*
+        public bool populatePersistentStorage(Dictionary<string, Dictionary<string, string>> dynamicStorage)
         {
-            if (!userExists(username))
-            {
-                int saltLength = 8;
-                string salt = generateSalt(saltLength);
-                string hashedPassword = hasher.computeSha256Hash(password + salt);
 
-                using (StreamWriter sw = new StreamWriter(storageFilePath, true))
-                {
-
-                    string line = username + "," + hashedPassword + "," + salt + "," + email;
-                    sw.WriteLine(line);
-                    sw.Flush();
-                }
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        // Storage of passwords needs work, also this needs to be broken up*******************************************************************************************************************
-        public void updatePassword(string userOrEmail, string newPassword)
-        {
-            int credentialIndex = 0;
-
-            if (RandomFunctions.isValidEmail(userOrEmail))
-            {
-                credentialIndex = 3;
-            }
-            else
-            {
-                credentialIndex = 0;
-            }
-
-            string? line = string.Empty;
-            int lineToModify = -1;
-            string[] tokens = new string[4];
-
-            // Read through the user storage
-            using (StreamReader sr = new StreamReader(storageFilePath))
-            {
-                while ((line = sr.ReadLine()) != null)
-                {
-                    lineToModify++;
-                    tokens = line.Split(',');
-
-                    // Check if this line contains the username
-                    if (tokens[credentialIndex].CompareTo(userOrEmail) == 0)
-                    {
-                        break;
-                    }
-                }
-            }
-
-            // Takes the whole storage file and edits one line.
-            string[] wholeTextFile = File.ReadAllLines(storageFilePath);
-            if (lineToModify < wholeTextFile.Length)
-            {
-                wholeTextFile[lineToModify] = tokens[0] + ","+ hasher.computeSha256Hash(newPassword + tokens[2]) + "," + tokens[2] + "," + tokens[3];
-            }
+            wholeTextFile[lineToModify] = tokens[0] + "," + hasher.computeSha256Hash(newPassword + tokens[2]) + "," + tokens[2] + "," + tokens[3];
 
             // Writes the edited version back to the storage file.
             File.WriteAllLines(storageFilePath, wholeTextFile);
+            return true;
         }
-
-        public bool userExists(string username)
-        {
-            string? line = string.Empty;
-
-            // Read through the user storage
-            using (StreamReader sr = new StreamReader(storageFilePath))
-            {
-                while ((line = sr.ReadLine()) != null)
-                {
-                    string[] tokens = line.Split(',');
-
-                    // Check if this line contains the username
-                    if (tokens[0].CompareTo(username) == 0)
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        private string generateSalt(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            Random random = new Random();
-            StringBuilder result = new StringBuilder(length);
-
-            for (int i = 0; i < length; i++)
-            {
-                result.Append(chars[random.Next(chars.Length)]);
-            }
-
-            return result.ToString();
-        }
+        */
     }
 }
